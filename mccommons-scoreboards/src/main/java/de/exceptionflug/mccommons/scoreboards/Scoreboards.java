12bd14eb92d5a65ab68d31fa3cfc92a90592974b
Nico Britze <n.britze@exceptionflug.de>
2019-05-13 11:35:10 +0200

    Dynamic config board

--- mccommons-scoreboards/src/main/java/de/exceptionflug/mccommons/scoreboards/Scoreboards.java
+++ mccommons-scoreboards/src/main/java/de/exceptionflug/mccommons/scoreboards/Scoreboards.java
@@ -1,6 +1,5 @@
 package de.exceptionflug.mccommons.scoreboards;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.DisplaySlot;
 
--- mccommons-scoreboards/src/main/java/de/exceptionflug/mccommons/scoreboards/localized/LocalizedConfigBoard.java
+++ mccommons-scoreboards/src/main/java/de/exceptionflug/mccommons/scoreboards/localized/LocalizedConfigBoard.java
@@ -11,12 +11,14 @@ import de.exceptionflug.mccommons.core.Providers;
 import de.exceptionflug.mccommons.core.providers.LocaleProvider;
 import de.exceptionflug.mccommons.core.utils.FormatUtils;
 import de.exceptionflug.mccommons.scoreboards.*;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.DisplaySlot;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class LocalizedConfigBoard {
 
@@ -26,7 +28,7 @@ public class LocalizedConfigBoard {
     private final int id = ID_GENERATOR.incrementAndGet();
     private final BoardPacketAdapter packetAdapter = new BoardPacketAdapter();
     private final Scoreboard scoreboard = new Scoreboard();
-    private final Map<String, String> replacements = new HashMap<>();
+    private final Map<String, Function<AbstractBoardHolder, String>> replacements = new HashMap<>();
 
     public LocalizedConfigBoard(final ConfigWrapper configWrapper) {
         config = configWrapper;
@@ -51,19 +53,66 @@ public class LocalizedConfigBoard {
 
     public void format(final String... replacements) {
         final Map<String, String> repMap = FormatUtils.createReplacementMap(replacements);
-        for(final Objective objective : scoreboard.getObjectives()) {
-            for(final Score score : objective.getScores()) {
-                score.reset();
+        for(final String key : repMap.keySet()) {
+            this.replacements.put(key, repMap::get);
+        }
+        update();
+    }
+
+    public void format(final String key, Function<AbstractBoardHolder, String> replacer) {
+        replacements.put(key, replacer);
+        update();
+    }
+
+    public void update() {
+        for(final AbstractBoardHolder boardHolder : Scoreboards.getBoardHolders(scoreboard)) {
+            for(final Objective objective : scoreboard.getObjectives()) {
+                for(final String placeholder : getUsedPlaceHolders(config.getLocalizedString(Providers.get(LocaleProvider.class).provide(boardHolder.getUniqueId()), "Objectives." + objective.getName(), ".displayName", "&eObjective"))) {
+                    if(!Objects.equals(packetAdapter.getLastState().get(placeholder), replacements.get(placeholder).apply(boardHolder))) {
+                        boardHolder.accept(objective.createRenamePacket());
+                        break;
+                    }
+                }
+            }
+            for(final Objective objective : scoreboard.getObjectives()) {
+                for(final Score score : objective.getScores()) {
+                    final String key = score.getEntry().substring(3);
+                    final String[] split = key.split("\\.");
+                    final String localizedString;
+                    if(split[2].equals("custom")) {
+                        localizedString = config.getLocalizedString(Providers.get(LocaleProvider.class).provide(boardHolder.getUniqueId()), "Objectives." + split[1] + ".custom", "."+split[3], "&6Entry");
+                    } else {
+                        localizedString = config.getLocalizedString(Providers.get(LocaleProvider.class).provide(boardHolder.getUniqueId()), "Objectives." + split[1] + ".scores." + split[2], ".entry", "&6Entry");
+                    }
+                    for(final String placeholder : getUsedPlaceHolders(localizedString)) {
+                        if(!Objects.equals(packetAdapter.getLastState().get(placeholder), replacements.get(placeholder).apply(boardHolder))) {
+                            boardHolder.accept(score.createRemovePacket());
+                            boardHolder.accept(score.createSetScorePacket());
+                            break;
+                        }
+                    }
+                }
             }
         }
-        this.replacements.clear();
-        this.replacements.putAll(repMap);
-        for(final Objective objective : scoreboard.getObjectives()) {
-            scoreboard.broadcastPacket(objective.createRenamePacket());
-            for(final Score score : objective.getScores()) {
-                score.setScore(score.getScore());
+    }
+
+    private List<String> getUsedPlaceHolders(final String in) {
+        final List<String> out = new ArrayList<>();
+        boolean percent = false;
+        StringBuilder builder = new StringBuilder("%");
+        for(final char c : in.toCharArray()) {
+            if(c == '%') {
+                if(percent) {
+                    builder.append("%");
+                    out.add(builder.toString());
+                    builder = new StringBuilder("%");
+                }
+                percent = !percent;
+            } else if(percent) {
+                builder.append(c);
             }
         }
+        return out;
     }
 
     public Scoreboard getScoreboard() {
@@ -87,6 +136,8 @@ public class LocalizedConfigBoard {
 
     class BoardPacketAdapter extends PacketAdapter {
 
+        private Map<String, String> lastState;
+
         BoardPacketAdapter() {
             super(Providers.get(JavaPlugin.class), PacketType.Play.Server.SCOREBOARD_SCORE, PacketType.Play.Server.SCOREBOARD_OBJECTIVE);
         }
@@ -108,7 +159,11 @@ public class LocalizedConfigBoard {
                     } else {
                         localizedString = config.getLocalizedString(Providers.get(LocaleProvider.class).provide(event.getPlayer().getUniqueId()), "Objectives." + split[1] + ".scores." + split[2], ".entry", "&6Entry");
                     }
-                    scoreboardScore.setScoreName(FormatUtils.formatAmpersandColorCodes(FormatUtils.format(localizedString, replacements)));
+                    lastState = new HashMap<>();
+                    for(final String k : LocalizedConfigBoard.this.replacements.keySet()) {
+                        lastState.put(k, LocalizedConfigBoard.this.replacements.get(k).apply(new PlayerBoardHolder(event.getPlayer().getUniqueId())));
+                    }
+                    scoreboardScore.setScoreName(FormatUtils.formatAmpersandColorCodes(FormatUtils.format(localizedString, lastState)));
                     scoreboardScore.sendPacket(event.getPlayer());
                     event.setCancelled(true);
                 }
@@ -120,11 +175,19 @@ public class LocalizedConfigBoard {
                     if(scoreboard.getObjective(objective.getName()) == null)
                         return;
                     final String localizedString = config.getLocalizedString(Providers.get(LocaleProvider.class).provide(event.getPlayer().getUniqueId()), "Objectives." + objective.getName(), ".displayName", "&eObjective");
-                    objective.setDisplayName(FormatUtils.formatAmpersandColorCodes(FormatUtils.format(localizedString, replacements)));
+                    lastState = new HashMap<>();
+                    for(final String k : LocalizedConfigBoard.this.replacements.keySet()) {
+                        lastState.put(k, LocalizedConfigBoard.this.replacements.get(k).apply(new PlayerBoardHolder(event.getPlayer().getUniqueId())));
+                    }
+                    objective.setDisplayName(FormatUtils.formatAmpersandColorCodes(FormatUtils.format(localizedString, lastState)));
                     objective.sendPacket(event.getPlayer());
                     event.setCancelled(true);
                 }
             }
+        }
+
+        public Map<String, String> getLastState() {
+            return lastState;
         }
     }
 
