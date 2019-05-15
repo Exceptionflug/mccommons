22cef2c9b2ff1abd1a42dc237cfd94376ff44238
Nico Britze <n.britze@exceptionflug.de>
2019-05-15 15:10:38 +0200

    alert default api key

--- mccommons-config/mccommons-config-remote/mccommons-config-remote-client/src/main/java/de/exceptionflug/mccommons/config/remote/client/RemoteConfigClient.java
+++ mccommons-config/mccommons-config-remote/mccommons-config-remote-client/src/main/java/de/exceptionflug/mccommons/config/remote/client/RemoteConfigClient.java
@@ -8,12 +8,16 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 import java.util.concurrent.TimeUnit;
+import java.util.logging.LogManager;
 
 public class RemoteConfigClient {
 
     private final ConfigService configService;
 
     public RemoteConfigClient(final String baseUrl, final String apiKey) {
+        if(apiKey.equals("x7834HgsTSds9")) {
+            LogManager.getLogManager().getLogger("RemoteConfigClient").warning("[MCCommons] Using default api key for server communication! This is a security risk. Please change your api key in the application.properties file of your remote config server!");
+        }
         final OkHttpClient httpClient = new OkHttpClient.Builder().readTimeout(5, TimeUnit.SECONDS).connectTimeout(5, TimeUnit.SECONDS).addInterceptor(chain -> chain.proceed(chain.request().newBuilder().addHeader("X-API-KEY", apiKey).build())).build();
         final Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
