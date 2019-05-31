4b3aba6b354503bf3c9f918e5f031c213ff749c0
Nico Britze <n.britze@exceptionflug.de>
2019-05-31 13:53:59 +0200

    Allow v1_14_1

--- mccommons-inventories/mccommons-inventories-spigot/src/main/java/de/exceptionflug/mccommons/inventories/spigot/utils/ServerVersionProvider.java
+++ mccommons-inventories/mccommons-inventories-spigot/src/main/java/de/exceptionflug/mccommons/inventories/spigot/utils/ServerVersionProvider.java
@@ -9,6 +9,8 @@ public class ServerVersionProvider {
             String version = ReflectionUtil.getVersion();
             if(version.startsWith("v1_8")) {
                 protocolVersion = 47;
+            } else if(version.equals("v1_14_1")) {
+                protocolVersion = 480;
             } else {
                 throw new UnsupportedOperationException("Version "+version+" not supported yet.");
             }
