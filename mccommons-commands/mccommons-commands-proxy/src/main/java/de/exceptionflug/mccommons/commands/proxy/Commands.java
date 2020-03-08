0213c70c5ca511be4f60ce60093043a9a9335ec0
Leonhard <49833472+JavaFactoryDev@users.noreply.github.com>
2020-03-08 13:40:43 +0100

    Created Commands.java

---
+++ mccommons-commands/mccommons-commands-proxy/src/main/java/de/exceptionflug/mccommons/commands/proxy/Commands.java
@@ -0,0 +1,12 @@
+package de.exceptionflug.mccommons.commands.proxy;
+
+import lombok.experimental.UtilityClass;
+import net.md_5.bungee.api.plugin.Command;
+
+@UtilityClass
+public class Commands {
+    public void registerCommand(final Command command) {
+
+    }
+
+}
