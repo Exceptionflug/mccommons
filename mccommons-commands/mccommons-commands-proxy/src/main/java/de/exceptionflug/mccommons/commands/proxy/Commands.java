f65bb4b1ef3aa63d02f1c0dfbf18d8433504cb1b
Nico Britze <n.britze@exceptionflug.de>
2020-03-08 14:36:42 +0100

    Impl BungeeCord command class

--- mccommons-commands/mccommons-commands-proxy/src/main/java/de/exceptionflug/mccommons/commands/proxy/Commands.java
+++ mccommons-commands/mccommons-commands-proxy/src/main/java/de/exceptionflug/mccommons/commands/proxy/Commands.java
@@ -1,29 +1,54 @@
 package de.exceptionflug.mccommons.commands.proxy;
 
+import de.exceptionflug.mccommons.core.Providers;
 import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.Plugin;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
 
 @UtilityClass
 public class Commands {
 
     @SneakyThrows
     public void registerCommand(@NonNull final Command command) {
-
+        ProxyServer.getInstance().getPluginManager().registerCommand(Providers.get(Plugin.class), command);
     }
 
     public void removeCommands(final String... cmds) {
-
+        Arrays.stream(cmds)
+                .map(Commands::getCommand)
+                .filter(Objects::nonNull)
+                .forEach(command -> ProxyServer.getInstance().getPluginManager().unregisterCommand(command));
     }
 
     public boolean isRegistered(final String command) {
-        return false;
+        return getCommand(command) != null;
     }
 
     @SneakyThrows
     @SuppressWarnings("unchecked")
     private void removeCommand(final String command) {
-
+        Command cmd = getCommand(command);
+        if(cmd == null) {
+            return;
+        }
+        ProxyServer.getInstance().getPluginManager().unregisterCommand(cmd);
     }
+
+    private Command getCommand(String i) {
+        Command out = null;
+        for(Map.Entry<String, Command> entry : ProxyServer.getInstance().getPluginManager().getCommands()) {
+            if(entry.getValue().getName().equalsIgnoreCase(i)) {
+                out = entry.getValue();
+            }
+        }
+        return out;
+    }
+
 }
