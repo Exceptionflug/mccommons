9ffee286bdce833b8f9a3733b400480556f8695a
Nico Britze <n.britze@exceptionflug.de>
2020-03-21 10:49:35 +0100

    Remove example impl

--- mccommons-plugin/mccommons-plugin-spigot/src/main/java/de/exceptionflug/mccommons/plugin/spigot/MCCommonsSpigotBootstrap.java
+++ mccommons-plugin/mccommons-plugin-spigot/src/main/java/de/exceptionflug/mccommons/plugin/spigot/MCCommonsSpigotBootstrap.java
@@ -1,7 +1,6 @@
 package de.exceptionflug.mccommons.plugin.spigot;
 
 import com.flowpowered.nbt.CompoundTag;
-import de.exceptionflug.mccommons.commands.spigot.command.SpigotCommandFramework;
 import de.exceptionflug.mccommons.config.remote.client.RemoteConfigClient;
 import de.exceptionflug.mccommons.config.shared.ConfigFactory;
 import de.exceptionflug.mccommons.config.shared.ConfigItemStack;
@@ -27,7 +26,6 @@ import de.exceptionflug.mccommons.inventories.spigot.utils.ReflectionUtil;
 import de.exceptionflug.mccommons.inventories.spigot.utils.ServerVersionProvider;
 import de.exceptionflug.mccommons.plugin.spigot.commands.ConfigReloadCommand;
 import de.exceptionflug.mccommons.plugin.spigot.commands.HologramReloadCommand;
-import de.exceptionflug.mccommons.plugin.spigot.commands.SchafeCommand;
 import de.exceptionflug.mccommons.plugin.spigot.converter.ItemStackConverter;
 import de.exceptionflug.mccommons.plugin.spigot.converter.PlayerConverter;
 import org.bukkit.Bukkit;
@@ -84,11 +82,6 @@ public class MCCommonsSpigotBootstrap {
             Converters.register(ClickType.class, de.exceptionflug.mccommons.inventories.api.ClickType.class, new SpigotClickTypeConverter());
             Converters.register(InventoryType.class, org.bukkit.event.inventory.InventoryType.class, new SpigotInventoryTypeConverter());
             Converters.register(ReflectionUtil.getClass("{obc}.inventory.CraftItemStack"), ItemStack.class, new CraftItemStackConverter());
-
-            final ConfigWrapper rapper = ConfigFactory.create(new File("plugins/mccommons/commands/CommandFramework.yml"), SpigotConfig.class);
-            final SpigotCommandFramework commandFramework = new SpigotCommandFramework(rapper);
-
-            commandFramework.registerCommand(new SchafeCommand());
 
             Bukkit.getPluginManager().registerEvents(new InventoryListener(), plugin);
             plugin.getCommand("mcrl").setExecutor(new ConfigReloadCommand());
